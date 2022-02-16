4481e789a5bdc34ab9444a10cb081ab51588efb5
Lando Icaza <landoicco@gmail.com>
2022-02-16 05:43:24 -0600

    - res folder issue fixed! - loadSprites method working!

--- src/main/Game.java
+++ src/main/Game.java
@@ -1,23 +1,40 @@
 package main;
 
+import javax.imageio.ImageIO;
 import javax.swing.JFrame;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.io.InputStream;
 
 public class Game extends JFrame {
 
     private GameScreen gameScreen;
+    private BufferedImage img;
 
     public Game() {
-        setSize(600, 600);
+        importImage();
+        setSize(640, 640);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
         setVisible(true);
 
-        gameScreen = new GameScreen();
+        gameScreen = new GameScreen(img);
         add(gameScreen);
     }
 
     public static void main(String[] args) {
         System.out.println("Hola Mundo!");
         new Game();
+    }
+
+    private void importImage() {
+        InputStream is = getClass().getResourceAsStream("/spriteatlas.png");
+
+        try {
+            img = ImageIO.read(is);
+        } catch (IOException e) {
+            System.err.println("Image not found/available!");
+            e.printStackTrace();
+        }
     }
 }
--- src/main/GameScreen.java
+++ src/main/GameScreen.java
@@ -2,17 +2,37 @@ package main;
 
 import javax.swing.JPanel;
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.util.ArrayList;
 
 public class GameScreen extends JPanel {
 
-    public GameScreen() {
+    private BufferedImage img;
+    private ArrayList<BufferedImage> sprites = new ArrayList<>();
 
+    public GameScreen(BufferedImage img) {
+        this.img = img;
+        loadSprites();
+    }
+
+    private void loadSprites() {
+        for (int y = 0; y < 10; y++) {
+            for (int x = 0; x < 10; x++) {
+                sprites.add(img.getSubimage(x * 32, y * 32, 32, 32));
+            }
+        }
     }
 
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
 
-        g.drawRect(50, 50, 100, 100);
+        g.drawImage(sprites.get(29), 0, 0, null);
+
+//        for (int x = 0; x < 20; x++) {
+//            for (int y = 0; y < 19; y++) {
+//                g.drawRect(x * 32, y * 32, 32, 32);
+//            }
+//        }
     }
 }
